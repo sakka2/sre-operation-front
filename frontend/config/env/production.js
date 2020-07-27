d67f9a9cdcbf1635849fcea980f4963080933a76
mine <56010463+mine0507@users.noreply.github.com>
2020-07-27 10:00:16 +0900

    Update production.js

--- frontend/config/env/production.js
+++ frontend/config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_SERVER_HOST: 'http://tminegis.dps.ssk.ynwm.yahoo.co.jp',
+  API_SERVER_HOST: 'http://{#should set prod host}',
   API_SERVER_PORT: '8080',
   API_VERSION: 'v1'
 }
